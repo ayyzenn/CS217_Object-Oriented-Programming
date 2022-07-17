4b8db55ebb3f68db725c50073cda8a23e95e792f
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-07-17 09:05:14 +0500

    asdfkja;fsdjf

---
+++ Assingments/4/Fibonacci Numbers.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdlib.h> 
+using namespace std;
+
+
+//////// START OF MARKER FOR fib
+// Making a fibonacci series and then getting number with the help of index
+long fib(int index) 
+{
+    long number1 = 0; 
+    long number2 = 1;
+    long next_number = 0;
+    if (index < 2)
+    {
+        return index;
+    }
+    else 
+    {
+        for (int i = 0 ; i < index ; i += 1) 
+        {
+            next_number = number1 + number2;
+            number1 = number2;
+            number2 = next_number;
+        }
+        return number1;
+    }
+               
+}    
+//////// END OF MARKER 
+
+
+
+//////// DO NOT MODIFY CODE BEYOND THIS LINE
+
+int main() //int argc, char* argv[] 
+{
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+    cout << (fib(num)) <<endl;      //atoi(argv[1])
+    return 0;
+}
