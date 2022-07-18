5673a05b94fa0fe0030afe6ff5ac47c702fdee83
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-07-18 10:21:55 +0500

    aklsdfjad;ljfldjflj

---
+++ Books Codes/book_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    num = 6;
+    cout<<"My first C++ program." << endl; /* comment is being testing and with the help of this comment we can use it for mutltiple lines
+        like this one :)*/
+    cout<<"The sum of 2 and 3 = " << 5 << endl;
+    cout<<"7 + 8 = " << 7 + 8 << endl;
+    cout <<"Num = " << num << endl;
+    cout<<"\0";
+    return 0;
+}
