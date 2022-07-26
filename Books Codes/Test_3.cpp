6e5efefc13efad20df7ee1686330858d818c600f
Saad Ahmad <saad.ahmad1024@gmail.com>
2022-07-26 10:25:34 +0500

    sdf;lgk;fsdlk;lg

---
+++ Books Codes/Test_3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=2;
+    int y=4;
+    int z= --x + y--;
+    cout<<"The value of z is "<<z<<endl<<"The value of x is "<<x<<endl<<"The value of y is "<<y<<endl;
+}
